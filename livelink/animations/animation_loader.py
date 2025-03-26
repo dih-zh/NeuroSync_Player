7939eea46aa8dedb3e0ed02fd9c1df4162456b47
Steven Wolf <steven.wolf+git@pm.me>
2025-03-26 10:48:53 +0100

    use module path

--- livelink/animations/animation_loader.py
+++ livelink/animations/animation_loader.py
@@ -1,5 +1,9 @@
 import os
 import pandas as pd
+from pathlib import Path
+
+mod_path = Path(__file__).parent
+
 
 def load_animation(csv_path):
     """
@@ -7,39 +11,45 @@ def load_animation(csv_path):
     Returns the animation data as a NumPy array.
     """
     data = pd.read_csv(csv_path)
-    data = data.drop(columns=['Timecode', 'BlendshapeCount'])
+    data = data.drop(columns=["Timecode", "BlendshapeCount"])
     return data.values
 
+
 def load_emotion_animations(folder_path, blend_frames=16):
-    
     from livelink.animations.blending_anims import blend_animation_start_end
+
     animations = []
     if not os.path.isdir(folder_path):
         print(f"Directory {folder_path} does not exist.")
         return animations
     for file_name in os.listdir(folder_path):
-        if file_name.endswith('.csv'):
+        if file_name.endswith(".csv"):
             file_path = os.path.join(folder_path, file_name)
             animation = load_animation(file_path)
             if animation is not None:
                 try:
-                    blended = blend_animation_start_end(animation, blend_frames=blend_frames)
+                    blended = blend_animation_start_end(
+                        animation, blend_frames=blend_frames
+                    )
                     animations.append(blended)
                 except Exception as e:
                     print(f"Error blending animation {file_path}: {e}")
     return animations
 
+
 emotion_paths = {
-    "Angry": os.path.join("livelink", "animations", "Angry"),
-    "Disgusted": os.path.join("livelink", "animations", "Disgusted"),
-    "Fearful": os.path.join("livelink", "animations", "Fearful"),
-    "Happy": os.path.join("livelink", "animations", "Happy"),
-    "Neutral": os.path.join("livelink", "animations", "Neutral"),
-    "Sad": os.path.join("livelink", "animations", "Sad"),
-    "Surprised": os.path.join("livelink", "animations", "Surprised")
+    "Angry": os.path.join(mod_path, "Angry"),
+    "Disgusted": os.path.join(mod_path, "Disgusted"),
+    "Fearful": os.path.join(mod_path, "Fearful"),
+    "Happy": os.path.join(mod_path, "Happy"),
+    "Neutral": os.path.join(mod_path, "Neutral"),
+    "Sad": os.path.join(mod_path, "Sad"),
+    "Surprised": os.path.join(mod_path, "Surprised"),
 }
 
 emotion_animations = {}
 for emotion, folder in emotion_paths.items():
     emotion_animations[emotion] = load_emotion_animations(folder)
-    print(f"Loaded {len(emotion_animations[emotion])} animations for emotion '{emotion}'")
+    print(
+        f"Loaded {len(emotion_animations[emotion])} animations for emotion '{emotion}'"
+    )
